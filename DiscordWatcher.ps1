e1dede1d533b8699ac1a1bc466b25a38f6bca7ab
Facundo Silva <silva.facundo13@gmail.com>
2024-04-11 00:52:11 -0300

    feat: improve script make it more dynamic

--- DiscordWatcher.ps1
+++ DiscordWatcher.ps1
@@ -10,50 +10,67 @@ $folder = Join-Path -Path $userProfileFolder -ChildPath $relativeFolder
 # Listen only to Discord.exe file
 $filter ="Discord.exe"
 
-# Create Watcher
-$Watcher = New-Object IO.FileSystemWatcher $folder, $filter -Property @{ 
-	IncludeSubdirectories = $true
-	EnableRaisingEvents = $true
-}
-
-# Register the event
-$onCreatead = Register-ObjectEvent $Watcher -EventName "Created" -SourceIdentifier "DiscordDetection" -Action {
-	$path = $Event.SourceEventArgs.FullPath
-
-	Write-Host "New Discord.exe detected at $($path)"
-
-
-	$count = 0
-
-	# Wait for the process to be fully loaded
-	while ((Get-Process -Name "Discord" -ErrorAction SilentlyContinue | Where-Object {$_.Path -eq $path }) -eq $null) {
-		# Break if the process is not found after 20 attempts
-		If ($count -ge 20) {
-			break
-		}
-		$count++
-
-		Write-Host "Waiting new Discord to start... (Attempt: $count)"
-		Start-Sleep -Seconds 2
+try {
+	# Create Watcher
+	$Watcher = New-Object IO.FileSystemWatcher $folder, $filter -Property @{ 
+		IncludeSubdirectories = $true
+		EnableRaisingEvents = $true
 	}
 
-	If ($count -ge 20) {
-		Write-Host "Discord process not found..."
-	} Else {
-		Write-Host "New Discord Started..."
+	# Register the event
+	$handler = Register-ObjectEvent $Watcher -EventName "Created" -SourceIdentifier "DiscordDetection" -Action {
+		$path = $Event.SourceEventArgs.FullPath
 
-		# Loop through all programs in the System Tray
-		foreach ($GUID in (Get-ChildItem -Path 'HKCU:\Control Panel\NotifyIconSettings' -Name)) {
-			$ChildPath = "HKCU:\Control Panel\NotifyIconSettings\$($GUID)"
+		Write-Host "New Discord.exe detected at $($path)"
 
-			# Get the executable path of the current program
-			$Exec = (Get-ItemProperty -Path $ChildPath -Name ExecutablePath -ErrorAction SilentlyContinue).ExecutablePath
 
-			# If the program is discord then set the IsPromoted value to 1
-			if ($Exec -match "Discord") {
-				Set-ItemProperty -Path $ChildPath -Name IsPromoted -Value 1
+		$count = 0
+
+		# Wait for the process to be fully loaded
+		while ((Get-Process -Name "Discord" -ErrorAction SilentlyContinue | Where-Object {$_.Path -eq $path }) -eq $null) {
+			# Break if the process is not found after 20 attempts
+			If ($count -ge 20) {
+				break
+			}
+			$count++
+
+			Write-Host "Waiting new Discord to start... (Attempt: $count)"
+			Start-Sleep -Seconds 2
+		}
+
+		If ($count -ge 20) {
+			Write-Host "Discord process not found..."
+		} Else {
+			Write-Host "New Discord Started..."
+
+			# Loop through all programs in the System Tray
+			foreach ($GUID in (Get-ChildItem -Path 'HKCU:\Control Panel\NotifyIconSettings' -Name)) {
+				$ChildPath = "HKCU:\Control Panel\NotifyIconSettings\$($GUID)"
+
+				# Get the executable path of the current program
+				$Exec = (Get-ItemProperty -Path $ChildPath -Name ExecutablePath -ErrorAction SilentlyContinue).ExecutablePath
+
+				# If the program is discord then set the IsPromoted value to 1
+				if ($Exec -match "Discord") {
+					Set-ItemProperty -Path $ChildPath -Name IsPromoted -Value 1
+				}
 			}
 		}
-	}
 
+	}
+	Write-Host "Listening for Discord.exe creation events..."
+	do {
+		Wait-Event -Timeout 1
+	} while ($true)
+} finally {
+	Write-Host "Stopping Discord Detection..."
+	# Unregister the event
+	Unregister-Event -SourceIdentifier "DiscordDetection"
+
+	$handler | Remove-Job
+
+	# Dispose the Watcher
+	$Watcher.Dispose()
+
+	Write-Host "Discord Detection Stopped..."
 }

0680172760cee5ccb5ca8f86175205e14463696f
Facundo Silva <silva.facundo13@gmail.com>
2024-04-14 21:03:05 -0300

    feat: improve install.ps1

--- install.ps1
+++ install.ps1
@@ -1,5 +1,17 @@
+$userProfileFolder = $env:USERPROFILE
+
+$folderPath = Join-Path -Path $userProfileFolder -ChildPath "\AppData\Local\DiscordTray"
+
+New-Item -ItemType Directory -Path $folderPath -Force
+
+$filePath = Join-Path -Path $folderPath -ChildPath "DiscordTray.ps1"
+
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/silvafacundo/DiscordTray/main/DiscordTray.ps1" -OutFile $filePath
+
 $trigger = New-JobTrigger -Once -At (Get-Date).AddMinutes(1) -RepetitionInterval (New-TimeSpan -Minutes 1) -RepetitionDuration ([timespan]::MaxValue)
 
 $options = New-ScheduledJobOption -RunElevated -StartIfOnBattery -ContinueIfGoingOnBattery
 
-Register-ScheduledJob -Name DiscordWatcher -FilePath C:\Proyectos\DiscordWatcher\DiscordWatcher.ps1 -Trigger $trigger -ScheduledJobOption $options
+Unregister-ScheduledJob -Name DiscordTray -ErrorAction SilentlyContinue
+
+Register-ScheduledJob -Name DiscordTray -FilePath $filePath -Trigger $trigger -ScheduledJobOption $options
